2f7ce78c891fff3367d1325fa58f375f8951cd6c
Lvrdsean <seanny813@optonline.net>
2024-11-10 18:28:27 -0500

    Using JWTs

--- data/src/routes/guard.rs
+++ data/src/routes/guard.rs
@@ -1,4 +1,7 @@
-use crate::database::users::{self, Entity as Users};
+use crate::{
+    database::users::{self, Entity as Users},
+    utils::jwt::is_valid,
+};
 use axum::{
     headers::{authorization::Bearer, Authorization, HeaderMapExt},
     http::{Request, StatusCode},
@@ -19,10 +22,11 @@ pub async fn guard<T>(mut request: Request<T>, next: Next<T>) -> Result<Response
         .get::<DatabaseConnection>()
         .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
     let user = Users::find()
-        .filter(users::Column::Token.eq(Some(token)))
+        .filter(users::Column::Token.eq(Some(token.clone())))
         .one(database)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    is_valid(&token)?;
 
     let Some(user) = user else {
         return Err(StatusCode::UNAUTHORIZED);
